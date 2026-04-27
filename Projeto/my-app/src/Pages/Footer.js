37c62f99128bb7204a0f0e7d089db660297703e3
nicolaszattar <nicolaszattar@gmail.com>
2026-04-27 14:53:30 +0100

    Correção conflito Footer

--- Projeto/my-app/src/Pages/Footer.js
+++ Projeto/my-app/src/Pages/Footer.js
@@ -1,11 +1,7 @@
 function Footer() {
   return (
     <footer className="bg-dark text-white text-center py-3 mt-5">
-<<<<<<< HEAD
       <p className="mb-0">© 2025 Feira do Jogo. Todos os direitos reservados.</p>
-=======
-      <p className="mb-0">2023 My App. All rights reserved.</p>
->>>>>>> a9bc60f8c1626d394235b569db6fa51350e2363b
     </footer>
   );
 }
